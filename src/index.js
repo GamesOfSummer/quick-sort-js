0433ee35e4380375fb469165da3b9cbd86eaec0e
Summer T <summer@summertime-development.com>
2023-02-18 07:13:36 -0600

    half working

--- src/index.js
+++ src/index.js
@@ -6,12 +6,26 @@ function quickSort(array) {
         return array;
     }
     else {
-        return [];
+        var pivotPoint = array.pop();
+        var leftSmallerArray = [];
+        var rightBiggerArray = [];
+        for (var i = 0; i < array.length; i++) {
+            if (array[i] <= pivotPoint) {
+                leftSmallerArray.push(array[i]);
+            }
+            else {
+                rightBiggerArray.push(array[i]);
+            }
+        }
+        quickSort(leftSmallerArray);
+        quickSort(rightBiggerArray);
+        var newArray = [];
+        return newArray.concat(leftSmallerArray, pivotPoint, rightBiggerArray);
     }
-    return array;
 }
 (0, helpers_1.consoleStart)();
 (0, helpers_1.validateFxn)(quickSort([2, 1]), [1, 2]);
+(0, helpers_1.validateFxn)(quickSort([1, 2, 3, 4, 5]), [1, 2, 3, 4, 5]);
 (0, helpers_1.validateFxn)(quickSort([1, 2, 3, 4, 5, 6]), [1, 2, 3, 4, 5, 6]);
 (0, helpers_1.validateFxn)(quickSort([3, 1, 2, 9, 6, 1]), [1, 1, 2, 3, 6, 9]);
 (0, helpers_1.consoleEnd)();
--- src/index.ts
+++ src/index.ts
@@ -5,25 +5,37 @@ import {
     validateFxn,
 } from './helpers';
 
-
-
 function quickSort(array: number[]) {
     if (array.length <= 1) {
         return array;
     } else {
+        const pivotPoint = array.pop();
 
-        
-        return [];
+        let leftSmallerArray = [];
+        let rightBiggerArray = [];
+
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] <= pivotPoint) {
+                leftSmallerArray.push(array[i]);
+            } else {
+                rightBiggerArray.push(array[i]);
+            }
+        }
+
+        quickSort(leftSmallerArray);
+        quickSort(rightBiggerArray);
+
+        let newArray = [];
+        return newArray.concat(leftSmallerArray, pivotPoint, rightBiggerArray);
     }
-
-    return array;
 }
 
 consoleStart();
 
-validateFxn(quickSort([2, 1]), [1, 2]);
+//validateFxn(quickSort([2, 1]), [1, 2]);
+//validateFxn(quickSort([1, 2, 3, 4, 5]), [1, 2, 3, 4, 5]);
 validateFxn(quickSort([1, 2, 3, 4, 5, 6]), [1, 2, 3, 4, 5, 6]);
-validateFxn(quickSort([3, 1, 2, 9, 6, 1]), [1, 1, 2, 3, 6, 9]);
+//validateFxn(quickSort([3, 1, 2, 9, 6, 1]), [1, 1, 2, 3, 6, 9]);
 
 consoleEnd();
 consoleBuffer();
